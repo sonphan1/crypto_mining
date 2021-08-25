a8676958509b83636b52d789ee2b01a6e0bd9893
sonphan1 <sonphan1@gmail.com>
2021-08-25 00:45:02 -0500

    update bat

--- PhoenixMiner_5.7b_Windows/start_miner.bat
+++ PhoenixMiner_5.7b_Windows/start_miner.bat
@@ -9,6 +9,7 @@ setx GPU_MAX_ALLOC_PERCENT 90
 setx GPU_SINGLE_ALLOC_PERCENT 90
 
 REM IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+REM PhoenixMiner.exe -pool eu1.ethermine.org:4444 -pool2 us1.ethermine.org:4444 -wal 0x0620d26d5dde3ec053aa0ac49404efcc8555a4a3.lance-pc
 PhoenixMiner.exe -pool eu1.ethermine.org:4444 -pool2 us1.ethermine.org:4444 -wal 0x0620d26d5dde3ec053aa0ac49404efcc8555a4a3.lance-pc
 pause
 
