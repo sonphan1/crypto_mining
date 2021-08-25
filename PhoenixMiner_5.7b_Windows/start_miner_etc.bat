ecead6183a50c67cc4e7bc0836250f07a8ec701a
sonphan1 <sonphan1@gmail.com>
2021-08-25 00:46:08 -0500

    update bat

--- PhoenixMiner_5.7b_Windows/start_miner_etc.bat
+++ PhoenixMiner_5.7b_Windows/start_miner_etc.bat
@@ -3,12 +3,12 @@ REM Example bat file for starting PhoenixMiner.exe to mine ETC
 REM
 
 setx GPU_FORCE_64BIT_PTR 0
-setx GPU_MAX_HEAP_SIZE 100
+setx GPU_MAX_HEAP_SIZE 60
 setx GPU_USE_SYNC_OBJECTS 1
-setx GPU_MAX_ALLOC_PERCENT 100
-setx GPU_SINGLE_ALLOC_PERCENT 100
+setx GPU_MAX_ALLOC_PERCENT 60
+setx GPU_SINGLE_ALLOC_PERCENT 60
 
 REM IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
-PhoenixMiner.exe -pool ssl://eu1-etc.ethermine.org:5555 -wal 0x008c26f3a2Ca8bdC11e5891e0278c9436B6F5d1E.Rig001 -coin etc
+PhoenixMiner.exe -pool eu1.ethermine.org:4444 -pool2 us1.ethermine.org:4444 -wal 0x0620d26d5dde3ec053aa0ac49404efcc8555a4a3.lance-pc
 pause
 
