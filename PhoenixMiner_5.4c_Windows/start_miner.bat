d17452d8ffd0b890ad1fbe85cc8e7a56c6266a36
Son <sonphan1@gmailcom>
2021-11-04 16:56:57 -0500

    using us1

--- PhoenixMiner_5.4c_Windows/start_miner.bat
+++ PhoenixMiner_5.4c_Windows/start_miner.bat
@@ -10,6 +10,6 @@ setx GPU_SINGLE_ALLOC_PERCENT 90
 
 REM IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
 REM PhoenixMiner.exe -pool eu1.ethermine.org:4444 -pool2 us1.ethermine.org:4444 -wal 0x0620d26d5dde3ec053aa0ac49404efcc8555a4a3.lance-pc
-PhoenixMiner.exe -pool eu1.ethermine.org:4444 -pool2 us1.ethermine.org:4444 -wal 0x0620d26d5dde3ec053aa0ac49404efcc8555a4a3.lance-pc
+PhoenixMiner.exe -pool us1.ethermine.org:4444 -pool2 eu1.ethermine.org:4444 -wal 0x0620d26d5dde3ec053aa0ac49404efcc8555a4a3.lance-pc
 pause
 
